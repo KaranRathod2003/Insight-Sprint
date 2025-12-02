f97605b0991628c9c861924f924c92ca2224fc31
Karan Rathod <kr04391@gmail.com>
2025-12-02 17:42:39 +0530

    add error middleware

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,25 @@
 import express from 'express';
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import { errorHandler } from './middlewares/error.middleware.js';
 
+
+dotenv.config();
 const app = express();
 
 
+app.use(cors({
+    origin : process.env.CORS_ORIGIN,
+    credentials: true,
+}))
+
+app.use(express.json({limit : "20kb"}));
+app.use(express.urlencoded({extended : true}));
+app.use(cookieParser());
+app.use(express.static("public"));
+
+app.use(errorHandler);
 
 
 
