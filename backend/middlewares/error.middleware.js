1154d7010253c721eb6f0af84c7b8e7778411d45
Karan Rathod <kr04391@gmail.com>
2025-12-02 17:41:19 +0530

    chore(core): add global error handler middleware

---
+++ backend/middlewares/error.middleware.js
@@ -0,0 +1,21 @@
+import { ApiError } from "../utils/ApiError";
+
+
+const errorHandler = async(err, req, res, next) =>{
+    console.error("Error : ", err);
+
+    if(err instanceof ApiError){
+        return res.status(err.statusCode).json({
+            success : false,
+            message : err.message,
+            errors : err.errors || []
+        })
+    }
+
+    return res.status(err.statusCode).json({
+        success : false,
+        message : "Internal server error"
+    })
+}
+
+export { errorHandler }
