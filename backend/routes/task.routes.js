4e03405f200778eb8245db3c3534c9c143edf8b6
Karan Rathod <kr04391@gmail.com>
2025-12-04 13:43:42 +0530

    feat(task) : add routes for task

---
+++ backend/routes/task.routes.js
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import { verifyJWT } from '../middlewares/auth.middleware.js';
+import { createTask, deleteTask, getTodayTasks, toggleTask } from '../controllers/task.controller.js';
+
+const router = Router();
+
+router.route('/').post(verifyJWT, createTask);
+
+router.route('/tasks/today').get(verifyJWT, getTodayTasks);
+
+router.route('/tasks/:id').patch(verifyJWT, toggleTask);
+
+router.route('/tasks/:id').delete(verifyJWT, deleteTask);
+
+
+
+
+export default router
