d7757e8756597f8d9c796521a2487880af95d0ad
Karan Rathod <kr04391@gmail.com>
2025-12-04 16:54:34 +0530

    feat(mood) : add logMood getweeklyMood controllers

---
+++ backend/controllers/mood.controller.js
@@ -0,0 +1,43 @@
+import { Mood } from '../models/mood.model.js';
+import { ApiError } from '../utils/ApiError.js'
+import { ApiResponse } from '../utils/ApiResponse.js'
+import { asyncHandler } from '../utils/asyncHandler.js'
+
+
+const logMood = asyncHandler(async(req, res)=>{
+    const {mood, note} = req.body;
+
+    if(!mood) throw new ApiError(404, "Mood field is required");
+
+    const date = new Date().toISOString().split("T")[0];
+
+    const moodModule = await Mood.create({
+        mood,
+        note, 
+        user : req.user._id,
+        date
+    })
+    return res.status(201).json(new ApiResponse(201, moodModule, "Mood created successfully"));
+})
+
+
+const getWeeklyMood  = asyncHandler(async(req, res)=>{
+
+    // 1. find todays date
+    const today = new Date();
+    // 2. last7th day date
+    const last7Days = new Date();
+    last7Days.setDate(today.getDate() - 7);
+
+    const last7DaysDate = last7Days.toISOString().split("T")[0];
+
+    const moods = Mood.find({
+        user : req.user._id,
+        date : {$gte: last7DaysDate}
+    })
+
+    return res.status(200).json(new ApiResponse(200, moods, "Weekly mood fetch successfully"));
+})
+
+
+export {logMood, getWeeklyMood}
